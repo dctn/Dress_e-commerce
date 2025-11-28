9427dedaa46de5c83b455e99438b33a0afcadc37
critictechsolutions-dot <critictechsolutions@gmail.com>
2025-11-28 14:28:56 +0000

    from vps

--- e_commerce_site/settings.py
+++ e_commerce_site/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["64.227.177.14","sanjayfashion.com","www.sanjayfashion.com"]
+DJANGO_CSRF_TRUSTED_ORIGINS=["64.227.177.14","https://sanjayfashion.com","https:/www.sanjayfashion.com"]
 
 # Application definition
 
